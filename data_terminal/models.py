64c667fab3cc4c1983930088d56305935b37dc9c
Besher Aboujeeb <besheraboujeeb@Beshers-MacBook-Pro.local>
2021-02-03 20:19:36 +0800

    added the model for the data terminal

--- data_terminal/models.py
+++ data_terminal/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
 # Create your models here.
+
+
+class data_terminal(models.Model):
+    SW = models.IntegerField()
+    T1 = models.IntegerField()
+    T3 = models.IntegerField()
+    T4 = models.IntegerField()
+    T5 = models.IntegerField()
+    TS = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- data_terminal/views.py
+++ data_terminal/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def showdata(request):
+    return render(request, showdata.html)
